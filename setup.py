98c1d435cb230d5083f98a8df3381cd019bfe652
Neeraj-GitHubDesk <neerajp330@gmail.com>
2025-05-29 22:15:38 +0200

    Phase 2 - 2. sSetup.py and pyproject.toml file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Neeraj Kumar",
+    author_email="neerajp330@gmail.com",
+    packages=find_packages()
+)
